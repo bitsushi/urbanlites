b556e690c635fe0be3b59b71206a01099cbc50fc
John Rees <john@bitsushi.com>
2012-05-10 17:58:00 +0100

    Quotes working

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,9 +2,10 @@ class Quote < ActiveRecord::Base
   # attr_accessible :author, :body, :organisation, :project_id, :ordinal, :position
   belongs_to :project
   default_scope order('id ASC')
+  validates_presence_of :body, :author
 
   before_save do
-    self.body = self.body.gsub!('"',"'")
+    self.body.gsub!('"',"'")
   end
 
 end
